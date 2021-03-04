ee501ea7750cdf99a950dd734e83cf3847a26808
Jim Knopf <jimknopf8jk@gmail.com>
2021-03-04 19:12:55 +0100

    now a thired+ screen should be posible

--- config/tags/init.lua
+++ config/tags/init.lua
@@ -5,11 +5,11 @@ local defaultApp = require('config.apps')
 
 -- local icons = require('theme.icons')
 
-_G.screen1 = 1
+_G.screens = {1}
 -- checks if there are as many screens as I want to have.
 -- in this case 2. for a third screen just change the 2 into a 3 an soon.
 -- If not this tag will added to the last screen
-_G.screen2 = (screen:count() >= 2 and 2 or screen:count())
+_G.screens[2] = (screen:count() >= 2 and 2 or screen:count())
 
 local tags = {
 	{
@@ -17,42 +17,42 @@ local tags = {
 		name = 'terminal',
 		type = 'terminal',
 		defaultApp = 'kitty',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	{
 		-- icon = icons.web_browser,
 		name = 'browser',
 		type = 'browser',
 		defaultApp = 'firefox',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	{
 		-- icon = icons.text_editor,
 		name = 'code',
 		type = 'code',
 		defaultApp = 'subl3',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	{
 		-- icon = icons.file_manager,
 		name = 'files',
 		type = 'files',
 		defaultApp = 'nemo',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	{
 		-- icon = icons.multimedia,
 		name = 'music',
 		type = 'music',
 		defaultApp = 'vlc',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	{
 		-- icon = icons.development,
 		name = 'any',
 		type = 'any',
 		defaultApp = '',
-		screen = _G.screen1
+		screen = _G.screens[1]
 	},
 	--
 	-- Screan 2
@@ -62,7 +62,7 @@ local tags = {
 		name = 'discord',
 	  	type = 'discord',
 	  	defaultApp = 'discord',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -70,7 +70,7 @@ local tags = {
 		name = 'code',
 	  	type = 'code',
 	  	defaultApp = 'code',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -78,7 +78,7 @@ local tags = {
 		name = 'terminal',
 	  	type = 'terminal',
 	  	defaultApp = defaultApp.terminal,
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -86,7 +86,7 @@ local tags = {
 		name = 'any',
 	  	type = 'any',
 	  	defaultApp = 'rofi',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -94,7 +94,7 @@ local tags = {
 		name = 'signal',
 	  	type = 'signal',
 	  	defaultApp = 'signal-desktop',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -102,7 +102,7 @@ local tags = {
 		name = 'thunderbird',
 	  	type = 'thunderbird',
 	  	defaultApp = 'thunderbird',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	},
 	{
@@ -110,7 +110,7 @@ local tags = {
 		name = 'telegram',
 	  	type = 'telegram',
 	  	defaultApp = 'telegram-desktop',
-	  	screen = _G.screen2,
+	  	screen = _G.screens[2],
 	  	layout = awful.layout.suit.tile.bottom
 	}
 }
@@ -121,25 +121,32 @@ awful.screen.connect_for_each_screen(
 		-- if you have more screens, tan you need to add heare some trues.
 		-- if you know a beatherway pleas contact me
 		local selected = {true,true,true,true,true,true}
-		for i, tag in ipairs(tags) do
-			-- checks on wich screen wich tag has to go
-			if s == screen[tag.screen] then
-				awful.tag.add(
-					tag.type, 
-					{
-						name				= tag.name,
-						icon 				= tag.icon,
-						icon_only 			= (tag.icon and true or false),
-						layout 				= (tag.layout and tag.layout or awful.layout.layouts[1]), -- tenaer operator (x?a:b)
-						gap_single_client	= false,
-						gap 				= 4,
-						screen 				= tag.screen,
-						defaultApp 			= tag.defaultApp,
-						selected 			= selected[tag.screen]
-					}
-				)
+		if _G.screens[s.index] then
+			for i, tag in ipairs(tags) do
+				-- checks on wich screen wich tag has to go
+				if tag.screen then -- ???
+				if s == screen[tag.screen] then
+					awful.tag.add(
+						tag.type, 
+						{
+							name				= tag.name,
+							icon 				= tag.icon,
+							icon_only 			= (tag.icon and true or false),
+							layout 				= (tag.layout and tag.layout or awful.layout.layouts[1]), -- tenaer operator (x?a:b)
+							gap_single_client	= false,
+							gap 				= 4,
+							screen 				= tag.screen,
+							defaultApp 			= tag.defaultApp,
+							selected 			= selected[tag.screen]
+						}
+					)
+				end
+				selected[tag.screen] = false
+				end
 			end
-			selected[tag.screen] = false
+		else
+    		-- Each screen has its own tag table.
+		    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 		end
   	end
 )
